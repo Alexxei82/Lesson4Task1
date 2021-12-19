98f32ae56b5a0a87424476ecf1941970c830c331
Алексей <@CEMEINYI>
2021-12-19 21:13:43 +0700

    Добавьте файлы проекта.

---
+++ Lesson4Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Возведение в квадрат положительного целого числа по формуле N2=1+3+5+...(2*N-1)
+        {
+            Console.WriteLine("Введите число > 0");
+               long n = Convert.ToInt64(Console.ReadLine()), sum = 0, x=1;
+            if (n>0)
+            {
+                for (long i = 1; i < (2 * n - 1) + 1; i += 2)
+                Console.WriteLine("Квадрат {0}равен {1}", x++, sum += i);
+            }
+            else Console.WriteLine("Вы ввели число меньше либо равное 0");
+                       
+            Console.ReadKey();
+
+            
+        }
+    }
+}
